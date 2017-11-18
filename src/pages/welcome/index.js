298f44eface63fe3a64d2830eec6c8ef17abe337
Werberson <werberson.silva@gmail.com>
2017-11-18 17:36:12 -0200

    navegando para abas

--- src/pages/welcome/index.js
+++ src/pages/welcome/index.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
 
+import { NavigationActions } from 'react-navigation';
+
+import PropTypes from 'prop-types';
+
 import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 
 import styles from './styles';
 
 export default class Welcome extends Component {
+  static propTypes = {
+    navigation: PropTypes.shape({
+      dispatch: PropTypes.func,
+    }).isRequired,
+  };
 
   static navigationOptions = {
     header: null,
+  };
+
+  navigateToUser = () => {
+    const { dispatch } = this.props.navigation;
+
+    const resetAction = NavigationActions.reset({
+      index: 0,
+      actions: [
+        NavigationActions.navigate({ routeName: 'User' }),
+      ],
+    });
+
+    dispatch(resetAction);
   };
 
   render() {
@@ -20,7 +42,7 @@ export default class Welcome extends Component {
 
         <TextInput style={styles.input} placeholder="Digite seu usuário" />
 
-        <TouchableOpacity style={styles.button} onPress={() => {}}>
+        <TouchableOpacity style={styles.button} onPress={this.navigateToUser}>
           <Text style={styles.buttonText}>Prosseguir</Text>
         </TouchableOpacity>
       </View>
