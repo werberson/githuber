3940a595fff3bdfff341865fe3e2dd2bf6158997
Werberson <werberson.silva@gmail.com>
2017-11-19 18:48:59 -0200

    rota inicial

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,38 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import 'config/ReactotronConfig';
 
 import React, { Component } from 'react';
 
-import 'config/ReactotronConfig';
-
-import Routes from 'routes';
+import createRootNavigator from 'routes';
+import { AsyncStorage } from 'react-native';
 
 
 export default class App extends Component<{}> {
+  state = {
+    userExists: false,
+    userChecked: false,
+  };
+
+  componentWillMount() {
+    console.tron.log('componentWillMount');
+    this.checkUser().then((response) => {
+      console.tron.log('checkUser.then');
+      this.setState({ userExists: response, userChecked: true });
+    });
+  }
+
+  checkUser = async () => {
+    console.tron.log('checkUser');
+    const user = await AsyncStorage.getItem('@Githuber:username');
+    return user !== null;
+  };
+
   render() {
-    return (
-      <Routes />
-    );
+    console.tron.log('render');
+    const { userChecked, userExists } = this.state;
+
+    if (!userChecked) return null;
+
+    const Layout = createRootNavigator(userExists);
+    return <Layout />;
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -4,16 +4,18 @@ import Welcome from 'pages/welcome';
 import Repositories from 'pages/repositories';
 import Organizations from 'pages/organizations';
 
-const Routes = StackNavigator({
-  Welcome: { screen: Welcome },
-  User: {
-    screen: TabNavigator({
-      Repositories: { screen: Repositories },
-      Organizations: { screen: Organizations },
-    }),
-  },
-}, {
-  initialRouteName: 'Welcome',
-});
 
-export default Routes;
+const createRootNavigator = (userExists = false) =>
+  StackNavigator({
+    Welcome: { screen: Welcome },
+    User: {
+      screen: TabNavigator({
+        Repositories: { screen: Repositories },
+        Organizations: { screen: Organizations },
+      }),
+    },
+  }, {
+    initialRouteName: userExists ? 'User' : 'Welcome',
+  });
+
+export default createRootNavigator;
